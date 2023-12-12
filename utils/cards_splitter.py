8d3f2a18686eacfccd75be0049ef571854477641
RYCBABAC <itay.chairman@gmail.com>
2023-12-12 20:50:15 +0200

    Fixed card splitter

--- utils/cards_splitter.py
+++ utils/cards_splitter.py
@@ -1,14 +1,9 @@
 from entities.card import Card
-from typing import List, Dict, Any, TypeVar
-from configuration_keys import ConfigurationKeys
-from icard_splitter import ICardsSplitter, Page, Row
+from typing import List, Dict, Any
+from utils.configuration_keys import ConfigurationKeys
+from utils.icard_splitter import ICardsSplitter, Page
 
-T = TypeVar("T")
-
-
-def split_list(lst: List[T], split_size: int) -> List[List[T]]:
-    for i in range(0, len(lst), split_size):
-        yield lst[i:i + split_size]
+PageWithoutRows = List[Card]
 
 
 class CardsSplitter(ICardsSplitter):
@@ -18,5 +13,14 @@ class CardsSplitter(ICardsSplitter):
         self._rows_in_page = configuration[ConfigurationKeys.NUM_ROWS_IN_PAGE_KEY]
 
     def split_cards(self, cards: List[Card]) -> List[Page]:
-        cards_in_rows: List[Row] = split_list(cards, self._cards_in_row)
-        return split_list(cards_in_rows, self._rows_in_page)
+        pages_without_rows: List[PageWithoutRows] = self.split_cards_to_pages_without_rows(cards)
+        return self.split_pages_into_rows(pages_without_rows)
+
+    def split_cards_to_pages_without_rows(self, cards: List[Card]) -> List[PageWithoutRows]:
+        cards_in_page: int = self._rows_in_page * self._cards_in_row
+        for i in range(0, len(cards), cards_in_page):
+            yield cards[i:i + cards_in_page]
+
+    def split_pages_into_rows(self, pages: List[PageWithoutRows]) -> List[Page]:
+        for page in pages:
+            yield [page[i: i + self._cards_in_row] for i in range(0, len(page), self._cards_in_row)]
